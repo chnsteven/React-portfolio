5bcbdfe6702f074bb968f914b64d19a49c764570
Steven Chen <chensteven0617@gmail.com>
2023-12-22 02:39:11 -0800

    added skill section to about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ScrollToTopButton from "../components/ScrollToTopButton";
 import { aboutContent } from "../utils/constants";
+const convertMarkdownToHTML = (text) => {
+  text = text.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>'); // Bold
+  text = text.replace(/\*(.*?)\*/g, '<em>$1</em>'); // Italic
+  return text;
+};
 function About() {
-  const convertMarkdownToHTML = (text) => {
-    text = text.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>'); // Bold
-    text = text.replace(/\*(.*?)\*/g, '<em>$1</em>'); // Italic
-    return text;
-  };
+  const [skills, setSkills] = useState([]);
+  useEffect(() => {
+    let skillsArray = [];
+    // Render each item in the array with dangerouslySetInnerHTML
+    aboutContent.forEach((content) => {
+      const container = document.createElement('div');
+      container.innerHTML = convertMarkdownToHTML(content);
+
+      // Use Array.from to convert NodeList to an array and then map to extract text content
+      const skillText = Array.from(container.querySelectorAll('strong')).map((strongTag) => strongTag.textContent);
+
+      // Join the strongTexts array into a sentence with commas
+      skillsArray.push(skillText.join(', '));
+    });
+    setSkills(skillsArray);
+  }, []);
 
   return (
     <div>
@@ -21,6 +37,13 @@ function About() {
             ))}
           </ul>
         {/* TODO: add skills */}
+        <h1 className="title">Skills</h1>
+        <ul>
+          {skills.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
+        </ul>
+
       </div>
     </div>
   );
--- server/Projects/cosmania.md
+++ server/Projects/cosmania.md
@@ -16,10 +16,10 @@ Self development project
 
 ## Features
 
-- **Self-taught Unity and C#, then developed a horror game that provides 5 minutes of non-repetitive gameplay.
-- **Integrated inkle into Unity to create a complex story system that allows players to make choices from multiple options.
-- **Resolved coupling and cohesion issues by implementing delegates and events for interactions among various game objects.
-- **Designed 2-4 cutscenes using Cinemachine timeline, incorporating character and camera movements.
+- Self-taught Unity and C#, then developed a horror game that provides 5 minutes of non-repetitive gameplay.
+- Integrated inkle into Unity to create a complex story system that allows players to make choices from multiple options.
+- Resolved coupling and cohesion issues by implementing delegates and events for interactions among various game objects.
+- Designed 2-4 cutscenes using Cinemachine timeline, incorporating character and camera movements.
 
 ## Images and Video
 
