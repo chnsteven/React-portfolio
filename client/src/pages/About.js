4fa1d95d7292d118efb2a7d60dc0bfcbac257457
Steven Chen <chensteven0617@gmail.com>
2023-12-22 01:33:01 -0800

    added about page with css styling

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,35 +1,26 @@
 import React from "react";
 import ScrollToTopButton from "../components/ScrollToTopButton";
+import { aboutContent } from "../utils/constants";
 function About() {
+  const convertMarkdownToHTML = (text) => {
+    text = text.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>'); // Bold
+    text = text.replace(/\*(.*?)\*/g, '<em>$1</em>'); // Italic
+    return text;
+  };
+
   return (
-    <div className="container">
+    <div>
       <ScrollToTopButton />
-      <h1 className="title">About</h1>
-      <div>
-        <ul>
-          <li>
-            Proficient in programming languages such as C/C++, Java, JS/TS,
-            Python, and R.
-          </li>
-          <li>
-            Experience with frameworks such as Node.js, Next.js, and Three.js.
-          </li>
-          <li>Experience with Test-driven developments, OOP, ECS.</li>
-          <li>Experience with version control with git.</li>
-          <li>Participated in a simulated Agile work environment.</li>
-          <li>
-            Understand design principles, design patterns, data structures and
-            algorithms.
-          </li>
-          <li>
-            Understand Intelligent system, computer graphics, and computer
-            animations.
-          </li>
-          {/* TODO: add skills */}
-          <li>
-            TODO: add skills
-          </li>
-        </ul>
+      <div className="container">
+        <h1 className="title">About</h1>
+          <ul>
+            {aboutContent.map((content, index) => (
+              <li key={index}>
+                <p key={index} dangerouslySetInnerHTML={{ __html: convertMarkdownToHTML(content) }} />
+              </li>
+            ))}
+          </ul>
+        {/* TODO: add skills */}
       </div>
     </div>
   );
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -21,3 +21,14 @@ export   const navigationLinks = [
   ];
 
   export const projectIds = ["Verloren", "Cosmania", "InsightUBC", "Java Application"];
+
+  export const aboutContent = [
+    "Proficient with programming languages such as **C**/**C++**, **Java**, **JS**/**TS**,**Python**, and **R**.",
+    "Experience with frameworks such as **Node.js**, **Next.js**, **React.js** and **Three.js**.",
+    "Experience with Test-driven developments, **OOP**, **ECS**.",
+    "Experience with version control with **git.",
+    "Participated in a simulated **Agile** work environment.",
+    "Understand **design principles**, **design patterns**, **data structures** and **algorithms**.",
+    "Understand **Intelligent** system, **computer graphics**, and **computer animations**.",
+    "Familiar with developing React application using **React-Router**, **Bootstrap**, **API calls**."
+  ]
